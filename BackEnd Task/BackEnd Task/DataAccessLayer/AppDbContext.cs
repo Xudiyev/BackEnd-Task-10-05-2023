0f92ae5d48de18b63b85a6378ce28488d265bbed
Arkan <arkankhudiyev@gmail.com>
2023-05-11 01:43:49 +0400

    Task

---
+++ BackEnd Task/BackEnd Task/DataAccessLayer/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd_Task.DataAccessLayer
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+    }
+}
--- BackEnd Task/BackEnd Task/Program.cs
+++ BackEnd Task/BackEnd Task/Program.cs
@@ -1,5 +1,13 @@
+using BackEnd_Task.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer("Server=DESKTOP-T2CJH2U\\SQLEXPRESS;Database=BackEndTaskDb;Trusted_Connection=true;");
+});
+
 var app = builder.Build();  
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
